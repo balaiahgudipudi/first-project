9bfae4140c12d52469ea2d292c92dbd0ed9dcc39
balaiahgudipudi <balaiahgudipudi@gmail.com>
2026-09-12 14:18:00 +0530

    Add files via upload

---
+++ Met.java
@@ -0,0 +1,16 @@
+class Met
+{
+	
+	public static void main(String args[])
+	{
+		int a[]=new int[5];
+		a[0]=10;
+		a[1]=20;
+		a[2]=30;
+		System.out.println(a[0]);
+		System.out.println(a[1]);
+		System.out.println(a[2]);
+		for(int i:a)
+		System.out.println(i);
+	}
+}
